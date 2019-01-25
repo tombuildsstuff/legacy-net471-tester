13cae4cd87e5597391fc045c43d0930128626145
tombuildsstuff <git@ibuildstuff.co.uk>
2019-01-25 22:25:00 +0000

    initial commit

---
+++ ExampleApp/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ExampleApp
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      var result = Environment.GetEnvironmentVariable("RESULT");
+      Assert.True(result == "pass");
+    }
+  }
+}
